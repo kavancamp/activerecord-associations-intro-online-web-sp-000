8f0fa94b7bfc478b9ae99812cb33a51bde491dcf
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-24 22:40:52 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,3 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 0) do
-
-end
