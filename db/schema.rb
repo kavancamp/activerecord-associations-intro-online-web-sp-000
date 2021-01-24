6789dee8684db34004a6d15eb7a0264adfce4e04
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-24 22:41:13 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -9,3 +9,21 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
+  end
+
+end
